680f1e97d0fffccaae9a849de42468419c8bac8d
unknown <scchethu@gmail.com>
2020-06-04 13:51:15 +0530

    updated

--- app/src/main/java/com/example/scram/farming/MainActivity.java
+++ app/src/main/java/com/example/scram/farming/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.scram.farming;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -17,13 +21,16 @@ import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
 public class MainActivity extends AppCompatActivity {
 Switch sw;
+View view;
+Button b1,b2,b3,b4;
+Intent intent;
  static String TOPIC="iot/xyz/pump";
-  final   static String SUB1="iot/xyz/a";
-  final   static String SUB2="iot/xyz/b";
-    final   static String SUB3="iot/xyz/c";
-    final   static String SUB4="iot/xyz/d";
-    final   static String SUB5="iot/xyz/e";
-    final   static String SUB6="iot/xyz/f";
+  final   static String SUB1="iot/sdm/a";//
+  final   static String SUB2="iot/sdm/b";//mois
+    final   static String SUB3="iot/sdm/c";//hum
+    final   static String SUB4="iot/sdm/d";//temp
+    final   static String SUB5="iot/sdm/e";//fan
+    final   static String SUB6="iot/sdm/f";//intr
     TextView gas,mois,hum,temp,fan,intruder;
 
     MqttAndroidClient c;
@@ -34,6 +41,44 @@ Switch sw;
         MemoryPersistence mPer = new MemoryPersistence();
         String clientId = MqttClient.generateClientId();
         String brokerUrl = "tcp://broker.mqtt-dashboard.com:1883";
+
+b1=findViewById(R.id.b1);
+b2=findViewById(R.id.b2);
+b3=findViewById(R.id.b3);
+b4=findViewById(R.id.b4);
+intent=new Intent(Intent.ACTION_VIEW);
+b1.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        intent.setData(Uri.parse("https://www.krishimaratavahini.kar.nic.in/"));
+        startActivity(intent);
+    }
+});
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent.setData(Uri.parse("https://pmkisan.gov.in/"));
+                startActivity(intent);
+            }
+        });
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent.setData(Uri.parse("https://farmer.gov.in/"));
+                startActivity(intent);
+            }
+        });
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                intent.setData(Uri.parse("https://agmarknet.gov.in/"));
+
+                startActivity(intent);
+            }
+        });
+
+
 
         c= new MqttAndroidClient(getApplicationContext(),brokerUrl, clientId);
         mois=findViewById(R.id.moisture);
@@ -83,23 +128,24 @@ Switch sw;
 
                 }
                 else if(topic.compareTo(SUB2)==0){
-                    temp.setText(new String(message.getPayload())+"°C");
+                    mois.setText(new String(message.getPayload())+"%");
+
 
                 }
                 else if(topic.compareTo(SUB3)==0){
-                    hum.setText(new String(message.getPayload()));
+                    hum.setText(new String(message.getPayload())+"%");
 
                 }
                 else if(topic.compareTo(SUB4)==0){
-                    gas.setText(new String(message.getPayload()));
+                    temp.setText(new String(message.getPayload())+"°C");
 
                 }
                 else if(topic.compareTo(SUB5)==0){
-                    intruder.setText(new String(message.getPayload()));
+                    fan.setText(new String(message.getPayload()));
 
                 }
                 else if(topic.compareTo(SUB6)==0){
-                    fan.setText(new String(message.getPayload()));
+                    intruder.setText(new String(message.getPayload()));
 
                 }
 
@@ -123,7 +169,7 @@ Switch sw;
                 if(isChecked){
                 // do something, the isChecked will be
 
-                String msg = "on";
+                String msg = "a";
                 MqttMessage m = new MqttMessage();
                 m.setPayload(msg.getBytes());
                     try {
@@ -134,7 +180,7 @@ Switch sw;
                 }
                 else {
 
-                    String msg = "off";
+                    String msg = "b";
                     MqttMessage m = new MqttMessage();
                     m.setPayload(msg.getBytes());
                     try {
